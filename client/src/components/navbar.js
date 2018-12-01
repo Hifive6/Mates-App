123af2f8b47809d16c379bd5a0f1791ac26be5fc
DKVaughn <dkvaughn1994@gmail.com>
2018-12-01 12:04:18 -0500

    resolved

---
+++ client/src/components/navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyOwnNavbar extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is my navbar</p>
+      </div>
+    )
+  }
+}
