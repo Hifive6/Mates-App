73d9c184fd63408e497da86bc3213f4972a27107
DKVaughn <dkvaughn1994@gmail.com>
2018-12-03 18:42:03 -0500

    react coponents

---
+++ client/src/components/dumb-components/navbar/navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Messageboard from '../messagebox/messageboard'
+import Jumbotronjs from '../Jumbotron/jumbotron';
+
+export default class myOwnNavbar extends Component {
+  render() {
+    return (
+      <div>
+
+        <nav className="nav-extended">
+          <div className="nav-wrapper">
+             <h1 className="brand-logo">Mates</h1>
+            
+           
+          </div>
+          <div className="nav-content">
+            <ul className="tabs tabs-transparent">
+              <li className="tab"><a href="#test1">Home</a></li>
+              <li className="tab"><a className="active" href="#test2">Bills</a></li>
+              <li className="tab"><a href="#test3">Chores</a></li>
+              <li className="tab"><a href="#test4">Profile</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div id="test1" className="col s12">
+        <Jumbotronjs />
+        <Messageboard /></div>
+        <div id="test2" className="col s12">Test 2</div>
+        <div id="test3" className="col s12">Test 3</div>
+        <div id="test4" className="col s12">Test 4</div>
+
+      </div>
+    )
+  }
+}
---
+++ client/src/components/dumb-components/signupbox/signupbox.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import '../signupbox/signup.css'
+
+import Carousel1 from '../carousel1/carousel1';
+export default class Signupbox extends Component {
+  render() {
+    return (
+      <div>
+        
+       
+<Carousel1 />
+       
+<h2>Sign-Up</h2>
+    <form>
+        <div className="form-group">
+            <label for="staticEmail" className="col-sm-2 col-form-label">Name</label>
+                <div className="col-sm-6">
+            <input type="text" className="form-control" id="name" value="text"></input>
+        </div>
+        <div className="form-group">
+            <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+                <div className="col-sm-6">
+            <input type="text" className="form-control" id="staticEmail" value="email@example.com"></input>
+          </div>
+        </div>
+        <div className="form-group">
+            <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                <div className="col-sm-6">
+            <input type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
+        </div>
+        <br></br>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+        </div>
+        </form>
+        
+
+      </div>
+    )
+  }
+}
