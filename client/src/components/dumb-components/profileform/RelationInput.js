7b68be2457c4421ddec4148eb0e606372839b11c
DKVaughn <dkvaughn1994@gmail.com>
2018-12-04 18:52:17 -0500

    profile component and profile page

---
+++ client/src/components/dumb-components/profileform/RelationInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class RlationInput extends Component {
+  render() {
+    return (
+      <div>
+         
+                    <label for="inputZip">Relation</label>
+                    <input type="text" class="form-control" id="inputZip"></input>
+               
+      </div>
+    )
+  }
+}
